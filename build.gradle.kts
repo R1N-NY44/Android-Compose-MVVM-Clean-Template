4395dd43e94030262131387fb24dca027b494a38
IGN. Brindawan Tri Guna Yoga <im.brindawan@gmail.com>
2024-05-08 02:54:45 +0700

    Setup Library version

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+
+    //ksp module
+    //id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false //old way
+    alias(libs.plugins.googleDevtoolsKsp) apply false
+}
